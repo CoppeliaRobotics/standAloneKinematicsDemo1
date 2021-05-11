2d453735a1450536bd7327ea31391eba8b458927
Coppelia <mfreese@gmx.ch>
2021-05-11 15:28:18 +0200

    different resource path on MacOS

--- standAloneKinematicsDemo1.cpp
+++ standAloneKinematicsDemo1.cpp
@@ -40,7 +40,11 @@ int main(int argc, char* argv[])
 
     // Read the kinematic file:
     FILE *file;
+#ifdef __APPLE__
+    file=fopen("../Resources/lbr_iiwa_7_r800.ik","rb");
+#else
     file=fopen("lbr_iiwa_7_r800.ik","rb");
+#endif
     unsigned char* data=NULL;
     int dataLength=0;
     if (file)
